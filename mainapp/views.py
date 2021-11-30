0cd12de2d27de4e0ed056f498cb577b1c8d0f69e
kubanych <sk-west@bk.ru>
2021-11-30 20:49:01 +0300

    add project

---
+++ mainapp/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+import json
+import os
+
+MODULE_DIR = os.path.dirname(__file__)
+
+
+# Create your views here.
+def index(request):
+    context = {
+        'title': 'Geekshop', }
+    return render(request, 'mainapp/index.html', context)
+
+
+def products(request):
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    context = {
+        'title': 'Geekshop - Каталог',
+    }
+    context['products'] = json.load(open(file_path, encoding='utf-8'))
+    return render(request, 'mainapp/products.html', context)
+
+
+def test(request):
+    context = {
+        'title': 'geekshop',
+        'header': 'Добро пожаловать на сайт',
+        'user': 'Николай',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6091},
+            {'name': 'Синяя куртка The North Face', 'price': 23726},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3391},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2341},
+            {'name': 'Черные туфли на платформе с 4 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2891},
+        ],
+
+        'products_promo': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 5001},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 2001},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 1001},
+
+        ]
+    }
+    return render(request, 'test_content.html', context)
