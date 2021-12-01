2cfb3956ba7fff190f52889acd6b13dd284581e6
kubanych <sk-west@bk.ru>
2021-12-01 13:49:23 +0300

    created {% for product in products %}

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -1,13 +1,12 @@
 import json
 
 from django.core.management.base import BaseCommand
-from mainapp.vodels import ProductCategory, Product
+from mainapp.models import ProductCategory, Product
+
 
 def load_from_json(file_name):
     with open(file_name, mode='r', encoding='utf-8') as infile:
-
-        retyrn json.load(infile)
-
+        return json.load(infile)
 
 
 class Command(BaseCommand):
@@ -16,18 +15,17 @@ class Command(BaseCommand):
 
         ProductCategory.objects.all().delete()
         for category in categories:
-            cat = categpry.get('fields')
+            cat = category.get('fields')
             cat['id'] = category.get('pk')
             new_category = ProductCategory(**cat)
             new_category.save()
 
         products = load_from_json('mainapp/fixtures/products.json')
 
-
         Product.objects.all().delete()
         for product in products:
             prod = product.get('fields')
-            categpry = product.get('categry')
+            category = product.get('categry')
             _category = ProductCategory.objects.get(id=category)
             prod['category'] = _category
             neq_category = Producr(**prod)
