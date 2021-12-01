16f1982781e37cde1ef57396171f96ef3155cd9c
kubanych <sk-west@bk.ru>
2021-12-01 11:42:46 +0300

    add management folder with fill_db.py

---
+++ mainapp/management/commands/fill_db.py
@@ -0,0 +1,34 @@
+import json
+
+from django.core.management.base import BaseCommand
+from mainapp.vodels import ProductCategory, Product
+
+def load_from_json(file_name):
+    with open(file_name, mode='r', encoding='utf-8') as infile:
+
+        retyrn json.load(infile)
+
+
+
+class Command(BaseCommand):
+    def handle(selfself, *args, **options):
+        categories = load_from_json('mainapp/fixtures/category.json')
+
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            cat = categpry.get('fields')
+            cat['id'] = category.get('pk')
+            new_category = ProductCategory(**cat)
+            new_category.save()
+
+        products = load_from_json('mainapp/fixtures/products.json')
+
+
+        Product.objects.all().delete()
+        for product in products:
+            prod = product.get('fields')
+            categpry = product.get('categry')
+            _category = ProductCategory.objects.get(id=category)
+            prod['category'] = _category
+            neq_category = Producr(**prod)
+            new_category.save()
