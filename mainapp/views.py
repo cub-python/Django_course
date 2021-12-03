12b1afc572a8f76a0e152850de8e46e620c0c4b1
kubanych <sk-west@bk.ru>
2021-12-03 19:06:43 +0300

    add

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# import json
+import json
 import os
 
 from mainapp.models import Product,ProductCategory
@@ -18,7 +18,7 @@ def index(request):
 def products(request):
     # file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
     context = {
-        'title': 'Geekshop - Каталог',
+        'title': 'Geekshop | Каталог',
     }
     # context['products'] = json.load(open(file_path, encoding='utf-8'))
     context['products'] = Product.objects.all()
@@ -26,25 +26,29 @@ def products(request):
     return render(request, 'mainapp/products.html', context)
 
 
-def test(request):
-    context = {
-        'title': 'geekshop',
-        'header': 'Добро пожаловать на сайт',
-        'user': 'Николай',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6091},
-            {'name': 'Синяя куртка The North Face', 'price': 23726},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3391},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': 2341},
-            {'name': 'Черные туфли на платформе с 4 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2891},
-        ],
 
-        'products_promo': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 5001},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 2001},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': 1001},
 
-        ]
-    }
-    return render(request, 'test_content.html', context)
+
+
+# def test(request):
+#     context = {
+#         'title': 'geekshop',
+#         'header': 'Добро пожаловать на сайт',
+#         'user': 'Николай',
+#         'products': [
+#             {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6091},
+#             {'name': 'Синяя куртка The North Face', 'price': 23726},
+#             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3391},
+#             {'name': 'Черный рюкзак Nike Heritage', 'price': 2341},
+#             {'name': 'Черные туфли на платформе с 4 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
+#             {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2891},
+#         ],
+#
+#         'products_promo': [
+#             {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 5001},
+#             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 2001},
+#             {'name': 'Черный рюкзак Nike Heritage', 'price': 1001},
+#
+#         ]
+#     }
+#     return render(request, 'test_content.html', context)
