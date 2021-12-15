8aa9b82d64df5ceeda187d5f476817f1e2301fda
kubanych <sk-west@bk.ru>
2021-12-15 19:38:09 +0300

    lesson_8

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,32 +1,36 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 import json
 import os
+
 from django.views.generic import DetailView
+
 from mainapp.models import Product, ProductCategory
 
 MODULE_DIR = os.path.dirname(__file__)
 
 
 # Create your views here.
+
 def index(request):
     context = {
         'title': 'Geekshop', }
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request,id_category=None,page = 1):
+def products(request,id_category=None,page=1):
 
     context = {
         'title': 'Geekshop | Каталог',
     }
 
     if id_category:
-        products = Product.objects.filter(category_id=id_category)
-
+        products= Product.objects.filter(category_id=id_category)
     else:
         products = Product.objects.all()
-    paginator = Paginator(products,per_pape=3)
+
+    paginator = Paginator(products,per_page=3)
 
     try:
         products_paginator = paginator.page(page)
@@ -41,17 +45,15 @@ def products(request,id_category=None,page = 1):
     return render(request, 'mainapp/products.html', context)
 
 
-class ProductDetail(DetailView):  #контролер вывода информации о продукте
+class ProductDetail(DetailView):
+
+    # Контроллер вывода информации о продукте
 
     model = Product
     template_name = 'mainapp/detail.html'
 
-    # context_object_name = 'product'
-
-    def get_context_data(self, **kwargs):
-        # Добваляем список категорий для вывода сайдбара с категориями на странице каталога
-        context = super(ProductDetali, self).get_context_data(**kwargs)
-        product = self.get_object()
-        context['product'] = product
-        return context
-
+    # def get_context_data(self, **kwargs):
+    #     context = super(ProductDetail, self).get_context_data(**kwargs)
+    #     product = self.get_object()
+    #     context['product'] = product
+    #     return context
