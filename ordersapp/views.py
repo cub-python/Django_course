fb1d4b573990711187adcffdc2c3117c8e13098a
kubanych <sk-west@bk.ru>
2022-01-08 21:47:15 +0300

    js

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -126,6 +126,7 @@ def get_product_price(request,pk):
             return JsonResponse({'price':product.price})
         return JsonResponse({'price':0})
 
+
 @receiver(pre_save, sender=Basket)
 @receiver(pre_save, sender=OrderItem)
 def product_quantity_update_save(sender, instance, **kwargs):
